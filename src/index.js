540ecf3751bd699f637ddb064f9333ce037c25ee
Mohammadullah Hasani <mh227jg@student.lnu.se>
2026-09-11 12:23:04 +0200

    Add first iteration of word game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Hej på er!')
+import readline from 'node:readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const wordLibrary = ['home', 'lamb', 'ears', 'cars']
+
+
+const currentWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)]
+const currentWordSplit = currentWord.split('')
+console.log(currentWordSplit[0])
+
+
+
+rl.question('Enter a word:', async (input) => {
+
+  const inputSplit = input.split('')
+
+  let checkWord = ''
+  for (let i = 0; i < currentWordSplit.length; i++) {
+
+    let checkLetter
+    if (currentWordSplit.includes(inputSplit[i])) {
+      checkLetter = '0'
+    }
+    
+    if
+      (inputSplit[i] === currentWordSplit[i]) {
+      checkLetter = 'x'
+    } else {
+      checkLetter = '_'
+    }
+    checkWord += checkLetter
+  }
+
+  console.log(checkWord)
+})
+
