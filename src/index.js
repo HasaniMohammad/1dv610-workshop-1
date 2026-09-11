873d9db3d10e2f4e563af7d71ab9d76994b9479e
Mattias Mellin <mm225vh@student.lnu.se>
2026-09-11 13:53:21 +0200

    Separate code into functions

--- src/index.js
+++ src/index.js
@@ -5,36 +5,71 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-const wordLibrary = ['home', 'lamb', 'ears', 'cars']
+try {
+  const argv = process.argv.splice(2)
+  const currentWord = await fetchRandomWord(argv[0])
+  const currentWordSplit = currentWord.split('')
 
+  let maxTries = argv[1]
+  let tries = 0
+  
+  playGame()
+} catch (err) {
+  console.error(err)
+  process.exit(err.errorCode || 1)
+}
 
-const currentWord = wordLibrary[Math.floor(Math.random() * wordLibrary.length)]
-const currentWordSplit = currentWord.split('')
-console.log(currentWordSplit[0])
+async function fetchRandomWord (length) {
+  const response = await fetch(`https://random-word-api.herokuapp.com/word?length=${length}`)
+  const data = await response.json()
 
+  return data[0]
+}
 
-
-rl.question('Enter a word:', async (input) => {
-
+function checkWord (input) {
   const inputSplit = input.split('')
 
   let checkWord = ''
   for (let i = 0; i < currentWordSplit.length; i++) {
 
     let checkLetter
-    if (currentWordSplit.includes(inputSplit[i])) {
+    if (inputSplit[i] === currentWordSplit[i]) {
+      checkLetter = 'X'
+    } else if (currentWordSplit.includes(inputSplit[i])) {
       checkLetter = '0'
-    }
-    
-    if
-      (inputSplit[i] === currentWordSplit[i]) {
-      checkLetter = 'x'
     } else {
       checkLetter = '_'
     }
+
     checkWord += checkLetter
   }
 
-  console.log(checkWord)
-})
+  return checkWord
+}
+
+async function playGame() {
+  rl.question('Enter a word: ', async (input) => {
+
+    if (input.length !== currentWord.length) {
+      console.log("Not correct letter length")
+      playGame()
+    }
+
+    let result = checkWord(input)
+
+    console.log(result)
+    tries++
+
+    if (currentWord === input) {
+      console.log('Correct Answer!')
+      rl.close()
+    } else if (tries === 6) {
+      console.log('You lost!')
+      rl.close()
+    } else {
+      playGame()
+    }
+  })
+}
+
 
